d0035765dd8bc5c69e22436e4abe4e7cfbdca3fc
shivam27 <shivampurohit900@gmail.com>
2023-09-29 19:53:15 +0530

    using restful api

--- project1/index.js
+++ project1/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+const fs = require('fs');
 
 const users = require('./MOCK_DATA.json');
+
+//middleware
+app.use(express.urlencoded({extended: false}));
 
 app.route('/api/users/:id')
 .get( (req, res)=>{
@@ -10,16 +14,33 @@ app.route('/api/users/:id')
     const user = users.find((user) => user.id ==id)
     return res.json(user);
 })
-.put( (req, res) => {
-    return res.json({status:"pending"});
-})
 .delete( (req, res) => {
-    return res.json({status:"pending"});
+    const userIdToDelete= Number(req.params.id);
+
+    const userIndex = users.findIndex((user) => user.id === userIdToDelete);
+
+    if (userIndex === -1) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    users.splice(userIndex, 1)
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
+        res.json({status:`deleted id ${req.params.id}`})
+    })
 })
 .patch( (req, res) => {
     return res.json({status:"pending"});
 })
 
+
+app.post('/api/users', ( (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length +1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
+        
+        return res.json({status:"success ", id: users.length});
+    });
+}) )
 app.get('/users', (req, res)=>{
     const html = `
     <ul>
